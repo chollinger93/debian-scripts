3e6eab4ede9e62d4ce6e1885e0a746d8ba0076b4
Christian Hollinger <christian.hollinger@otter-in-a-suit.com>
2019-12-17 10:59:57 -0500

    Package installed typo fixed

--- util/package_installed.sh
+++ util/package_installed.sh
@@ -3,7 +3,7 @@ isInstalled(){
 	PACKAGE=$1
 	echo "Checking Package ${PACKAGE}"
 	if [[ -z $(dpkg -l "${PACKAGE}") ]]; then
-		>&2 echo "youtube-dl is not installed"
+		>&2 echo "${PACKAGE} is not installed"
 		>&2 echo "install with:"
 		>&2 echo "sudo apt-get install ${PACKAGE}"
 		return 1
