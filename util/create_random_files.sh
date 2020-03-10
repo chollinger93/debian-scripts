ad56092bdb95965ea84fc602d91ea43a2198a23b
Christian Hollinger <christian.hollinger@otter-in-a-suit.com>
2020-03-10 17:21:57 -0400

    Random files requires arguments to control size, output

--- util/create_random_files.sh
+++ util/create_random_files.sh
@@ -2,10 +2,18 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${DIR}/logging.sh"
 
+if [[ $# -ge 2 ]]; then
+    THRESHOLD=$1
+    ROOT_DIR=$2
+else
+    logErr "Arguments: \$threshold \$output_dir"
+    exit 1
+fi
+
 # Creates random files 
-ROOT_DIR="/home/${USER}/testfiles/"
+#ROOT_DIR="/home/${USER}/testfiles/"
 mkdir -p $ROOT_DIR
-for n in {1..10}; do
+for ((i=0; i<=THRESHOLD; i++)); do
     RANDOM_PATH="${ROOT_DIR}${RANDOM}/"
     RANDOM_FILE="${RANDOM}"
     FILE_SIZE=$(( RANDOM + 1024 ))
